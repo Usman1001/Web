73ffecb8d3f6a6771a966ee3388d78514703ec7a
Usman <m.a.usman0776789108@gmail.com>
2023-08-21 19:33:39 +0530

    5
    21/08/2023

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
-const textArray = ["project", "fun", "free stuff", "tutorial"];
+const textArray = ["project", "Website", "Interface", "site"];
 const typingDelay = 100;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
